717bb6c92e14a6dea799a0694a2604ef5d34a7da
xavierloos <xavier.loos04@gmail.com>
2020-11-25 15:13:17 +0000

    8- Back to the unit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,7 @@
 class DockingStation 
   def release_bike
+    working_bike = true
+    print "Release bike"
   end
+  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,12 @@
 require "../lib/docking_station"
 
 describe DockingStation do
+
+    # it "release_bike" do
+    #   expect(working_bike(true)).to eq 'Release bike'
+    # end
+    it "release_bike" do
+      # expect(release('bike')).to eq 'is bike working?'
+    end
+
 end
