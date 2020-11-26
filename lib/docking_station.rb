8a05bd6537421688be396c5fcca8bcef6068674c
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-26 15:30:35 +0000

    empty docking stations no longer release bikes, bikes released are the ones docked

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,11 +2,12 @@ require_relative './bike.rb'
 
 class DockingStation
   attr_reader :bike
-  
+
   def release_bike
-    Bike.new
+    fail "No bikes available" if @bike == nil
+    @bike
   end
-  
+
   def dock_bike(bike)
     @bike = bike
   end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,17 @@
 require "../lib/docking_station"
 
 describe DockingStation do
+
   describe "#release_bike" do
-    it "releases a working bike" do
-      bike = subject.release_bike
-      expect(bike.working?).to be true
+    it "releases a working bike when a bike is available" do
+      original_bike = Bike.new
+      station = DockingStation.new
+      station.dock_bike(original_bike)
+      new_bike = station.release_bike
+      expect(new_bike.working?).to be true
+    end
+    it "returns an error when called on an empty docking station" do
+      expect {DockingStation.new.release_bike}.to raise_error("No bikes available")
     end
   end
 
